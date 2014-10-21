3d7884620d9f49ad1e1d26a4b399599a9225078a
uitgeholzerd <uitgeholzerd@gmail.com>
2014-10-21 16:41:08 +0200

    fixed scripts

--- update.sh
+++ update.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo Getting latest version...
+cd "$DIR/DS-Nameserver"
 git pull ds master
 mkdir "$DIR/DS-Nameserver/bin" 2> /dev/null
 echo Compiling...
-javac -d "$DIR/bin/" "$DIR/src/be/uantwerpen/ds/ns/*.java" "$DIR/src/be/uantwerpen/ds/ns/client/*.java" "$DIR/src/be/uantwerpen/ds/ns/server/*.java" "$DIR/src/be/uantwerpen/ds/test/*.java"
+javac -d $DIR/DS-Nameserver/bin/ $DIR/DS-Nameserver/src/be/uantwerpen/ds/ns/*.java $DIR/DS-Nameserver/src/be/uantwerpen/ds/ns/client/*.java $DIR/DS-Nameserver/src/be/uantwerpen/ds/ns/server/*.java $DIR/DS-Nameserver/src/be/uantwerpen/ds/test/*.java
 
