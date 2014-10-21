9834b02ff274276e8af671d6df9235b69342d432
uitgeholzerd <uitgeholzerd@gmail.com>
2014-10-22 00:30:56 +0200

    added init script

---
+++ init.sh
@@ -0,0 +1,23 @@
+if [ "$(id -u)" != "0" ]; then
+ echo "Are you root?" 
+ exit 1;
+fi
+if [ -z "$1" ]
+then
+ echo Usage: $0 newname
+ exit 1
+fi
+add-apt-repository ppa:webubd8team/java
+apt-get-update
+apt-get install oracle-jdk7-installer
+
+echo 'alias ds-client=~/ds-practicum/client.sh' > ~/.bashrc
+echo 'alias ds-server=~/ds-practicum/server.sh' > ~/.bashrc
+
+OLD=$(hostname)
+NEW=$1
+
+sed -i s/$OLD/$NEW/g /etc/hostname
+sed -i s/$OLD/$NEW/g /etc/hosts
+
+
