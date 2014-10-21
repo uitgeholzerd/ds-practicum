a4f3894ba84c024a3c2b2a4d1b57efc278af4d28
uitgeholzerd <uitgeholzerd@gmail.com>
2014-10-21 15:41:52 +0200

    changed server.sh to fix rmi issue

--- server.sh
+++ server.sh
@@ -1,2 +1,3 @@
 bash update.sh
-java -classpath DS-Nameserver/bin/ be.uantwerpen.ds.test.NameServerTest
+IP=$(ip -4 a show dev eth0 | sed -En 's/.*inet (addr:)?(([0-9]+\.){3}[0-9])+.*/\2/p')
+java -Djava.rmi.server.hostname=$IP -classpath DS-Nameserver/bin/ be.uantwerpen.ds.test.NameServerTest
--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 echo Getting latest version...
 git pull ds master
 cd DS-Nameserver
+mkdir bin
 echo Compiling...
 javac -d bin/ src/be/uantwerpen/ds/ns/*.java src/be/uantwerpen/ds/ns/client/*.java src/be/uantwerpen/ds/ns/server/*.java src/be/uantwerpen/ds/test/*.java 
 
