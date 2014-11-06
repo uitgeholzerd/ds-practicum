724b7c04a526c75bc364aa135350b4a4f5934e89
uitgeholzerd <uitgeholzerd@gmail.com>
2014-11-06 13:22:20 +0100

    added agent package to script

--- update.sh
+++ update.sh
@@ -5,5 +5,5 @@ cd "$DIR/DS-SystemY"
 git pull ds master
 mkdir "$DIR/DS-SystemY/bin" 2> /dev/null
 echo Compiling...
-javac -d $DIR/DS-SystemY/bin/ $DIR/DS-SystemY/src/be/uantwerpen/ds/system_y/*.java $DIR/DS-SystemY/src/be/uantwerpen/ds/system_y/client/*.java $DIR/DS-SystemY/src/be/uantwerpen/ds/system_y/server/*.java $DIR/DS-SystemY/src/be/uantwerpen/ds/system_y/test/*.java $DIR/DS-SystemY/src/be/uantwerpen/ds/system_y/connection/*.java
+javac -d $DIR/DS-SystemY/bin/ $DIR/DS-SystemY/src/be/uantwerpen/ds/system_y/agents/*.java $DIR/DS-SystemY/src/be/uantwerpen/ds/system_y/client/*.java $DIR/DS-SystemY/src/be/uantwerpen/ds/system_y/server/*.java $DIR/DS-SystemY/src/be/uantwerpen/ds/system_y/test/*.java $DIR/DS-SystemY/src/be/uantwerpen/ds/system_y/connection/*.java
 
