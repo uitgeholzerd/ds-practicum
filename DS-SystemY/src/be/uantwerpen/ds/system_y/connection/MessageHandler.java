99b81fcd23de1e8f2c53107678e3ff0ca136f33a
Girmi Schouten <girmi.schouten@gmail.com>
2014-11-26 14:26:17 +0100

    TestAgent back in the game

--- DS-SystemY/src/be/uantwerpen/ds/system_y/connection/MessageHandler.java
+++ DS-SystemY/src/be/uantwerpen/ds/system_y/connection/MessageHandler.java
@@ -90,7 +90,7 @@ public class MessageHandler {
 			System.out.println("I'm all alone, setting next and previous node to myself ("+ client.getHash() + ")");
 			client.setNextNodeHash(client.getHash());
 			client.setPreviousNodeHash(client.getHash());
-			//client.receiveAgent(new TestAgent());
+			client.receiveAgent(new TestAgent());
 		} else { 
 			try {
 				group.sendMessage(Protocol.NODE_JOINED, client.getName() + " " + client.getAddress().getHostAddress());
